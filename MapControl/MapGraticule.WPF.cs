99e218580df00dfa52275af7b509727acf6044aa
ClemensF <fischer.clemens@gmail.com>
2013-10-22 18:18:47 +0200

    Version 1.6.0: Retargeted WinRT version to 8.1. Improved MapGraticule performance for WPF.

--- MapControl/MapGraticule.WPF.cs
+++ MapControl/MapGraticule.WPF.cs
@@ -3,6 +3,7 @@
 // Licensed under the Microsoft Public License (Ms-PL)
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Media;
@@ -11,13 +12,27 @@ namespace MapControl
 {
     public partial class MapGraticule : MapOverlay
     {
+        private class Label
+        {
+            public readonly double Position;
+            public readonly string Text;
+
+            public Label(double position, string text)
+            {
+                Position = position;
+                Text = text;
+            }
+        }
+
+        private Dictionary<string, GlyphRun> glyphRuns = new Dictionary<string, GlyphRun>();
+
         static MapGraticule()
         {
             UIElement.IsHitTestVisibleProperty.OverrideMetadata(
                 typeof(MapGraticule), new FrameworkPropertyMetadata(false));
 
             MapOverlay.StrokeThicknessProperty.OverrideMetadata(
-                typeof(MapGraticule), new FrameworkPropertyMetadata(0.5));
+                typeof(MapGraticule), new FrameworkPropertyMetadata(0.5, (o, e) => ((MapGraticule)o).glyphRuns.Clear()));
         }
 
         protected override void OnViewportChanged()
@@ -40,45 +55,79 @@ namespace MapControl
                     spacing = LineSpacings.FirstOrDefault(s => s >= minSpacing);
                 }
 
-                var labelsStart = new Location(
+                var labelFormat = spacing < 1d ? "{0} {1}°{2:00}'" : "{0} {1}°";
+                var labelStart = new Location(
                     Math.Ceiling(start.Latitude / spacing) * spacing,
                     Math.Ceiling(start.Longitude / spacing) * spacing);
 
-                for (var lat = labelsStart.Latitude; lat <= end.Latitude; lat += spacing)
+                var latLabels = new List<Label>(16);
+                var lonLabels = new List<Label>(16);
+
+                for (var lat = labelStart.Latitude; lat <= end.Latitude; lat += spacing)
                 {
+                    latLabels.Add(new Label(lat, CoordinateString(lat, labelFormat, "NS")));
+
                     drawingContext.DrawLine(Pen,
                         ParentMap.LocationToViewportPoint(new Location(lat, start.Longitude)),
                         ParentMap.LocationToViewportPoint(new Location(lat, end.Longitude)));
                 }
 
-                for (var lon = labelsStart.Longitude; lon <= end.Longitude; lon += spacing)
+                for (var lon = labelStart.Longitude; lon <= end.Longitude; lon += spacing)
                 {
+                    lonLabels.Add(new Label(lon, CoordinateString(Location.NormalizeLongitude(lon), labelFormat, "EW")));
+
                     drawingContext.DrawLine(Pen,
                         ParentMap.LocationToViewportPoint(new Location(start.Latitude, lon)),
                         ParentMap.LocationToViewportPoint(new Location(end.Latitude, lon)));
                 }
 
-                if (Foreground != null && Foreground != Brushes.Transparent)
+                if (Foreground != null && Foreground != Brushes.Transparent && latLabels.Count > 0 && lonLabels.Count > 0)
                 {
-                    var format = spacing < 1d ? "{0} {1}°{2:00}'" : "{0} {1}°";
+                    var latLabelOrigin = new Point(StrokeThickness / 2d + 2d, -StrokeThickness / 2d - FontSize / 4d);
+                    var lonLabelOrigin = new Point(StrokeThickness / 2d + 2d, StrokeThickness / 2d + FontSize);
+                    var transform = Matrix.Identity;
+                    transform.Rotate(ParentMap.Heading);
 
-                    for (var lat = labelsStart.Latitude; lat <= end.Latitude; lat += spacing)
+                    foreach (var latLabel in latLabels)
                     {
-                        for (var lon = labelsStart.Longitude; lon <= end.Longitude; lon += spacing)
+                        foreach (var lonLabel in lonLabels)
                         {
-                            var t = StrokeThickness / 2d;
-                            var p = ParentMap.LocationToViewportPoint(new Location(lat, lon));
-                            var latPos = new Point(p.X + t + 2d, p.Y - t - FontSize / 4d);
-                            var lonPos = new Point(p.X + t + 2d, p.Y + t + FontSize);
-                            var latLabel = CoordinateString(lat, format, "NS");
-                            var lonLabel = CoordinateString(Location.NormalizeLongitude(lon), format, "EW");
+                            GlyphRun latGlyphRun;
+                            GlyphRun lonGlyphRun;
 
-                            drawingContext.PushTransform(new RotateTransform(ParentMap.Heading, p.X, p.Y));
-                            drawingContext.DrawGlyphRun(Foreground, GlyphRunText.Create(latLabel, Typeface, FontSize, latPos));
-                            drawingContext.DrawGlyphRun(Foreground, GlyphRunText.Create(lonLabel, Typeface, FontSize, lonPos));
+                            if (!glyphRuns.TryGetValue(latLabel.Text, out latGlyphRun))
+                            {
+                                latGlyphRun = GlyphRunText.Create(latLabel.Text, Typeface, FontSize, latLabelOrigin);
+                                glyphRuns.Add(latLabel.Text, latGlyphRun);
+                            }
+
+                            if (!glyphRuns.TryGetValue(lonLabel.Text, out lonGlyphRun))
+                            {
+                                lonGlyphRun = GlyphRunText.Create(lonLabel.Text, Typeface, FontSize, lonLabelOrigin);
+                                glyphRuns.Add(lonLabel.Text, lonGlyphRun);
+                            }
+
+                            var position = ParentMap.LocationToViewportPoint(new Location(latLabel.Position, lonLabel.Position));
+
+                            drawingContext.PushTransform(new MatrixTransform(
+                                transform.M11, transform.M12, transform.M21, transform.M22, position.X, position.Y));
+
+                            drawingContext.DrawGlyphRun(Foreground, latGlyphRun);
+                            drawingContext.DrawGlyphRun(Foreground, lonGlyphRun);
                             drawingContext.Pop();
                         }
                     }
+
+                    var removeKeys = glyphRuns.Keys.Where(k => !latLabels.Any(l => l.Text == k) && !lonLabels.Any(l => l.Text == k));
+
+                    foreach (var key in removeKeys.ToList())
+                    {
+                        glyphRuns.Remove(key);
+                    }
+                }
+                else
+                {
+                    glyphRuns.Clear();
                 }
             }
         }
