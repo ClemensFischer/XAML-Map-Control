37b42b3c1bd66543a71c53173e6c4e90cfda251f
ClemensF <fischer.clemens@gmail.com>
2018-05-09 18:14:27 +0200

    Version 4.9.0: Reworked MapPanel child arrangement.

--- MapControl/Shared/MapScale.cs
+++ MapControl/Shared/MapScale.cs
@@ -7,13 +7,11 @@ using System;
 using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Shapes;
 #else
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Shapes;
 #endif
