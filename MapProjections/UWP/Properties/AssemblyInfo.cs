fc9a5c980b956f3ef5dda3af99571b631f3da1e2
ClemensFischer <fischer.clemens@gmail.com>
2024-02-03 21:01:53 +0100

    Copyright note

--- MapProjections/UWP/Properties/AssemblyInfo.cs
+++ MapProjections/UWP/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("XAML Map Control Projections Library for UWP")]
 [assembly: AssemblyProduct("XAML Map Control")]
 [assembly: AssemblyCompany("Clemens Fischer")]
-[assembly: AssemblyCopyright("Copyright © 2023 Clemens Fischer")]
+[assembly: AssemblyCopyright("Copyright © 2024 Clemens Fischer")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyVersion("8.3.0")]
 [assembly: AssemblyFileVersion("8.3.0")]
