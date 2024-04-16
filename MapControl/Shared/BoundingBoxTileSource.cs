761bdf163f01fa4d95d34d760a37400bcdfcb130
ClemensFischer <fischer.clemens@gmail.com>
2024-04-16 22:30:21 +0200

    Update BoundingBoxTileSource.cs

--- MapControl/Shared/BoundingBoxTileSource.cs
+++ MapControl/Shared/BoundingBoxTileSource.cs
@@ -15,7 +15,7 @@ namespace MapControl
 
             if (UriTemplate != null)
             {
-                var tileSize = 360d / (1 << zoomLevel); // tile width in degrees
+                var tileSize = 360d / (1 << zoomLevel); // tile size in degrees
                 var west = MapProjection.Wgs84MeterPerDegree * (column * tileSize - 180d);
                 var east = MapProjection.Wgs84MeterPerDegree * ((column + 1) * tileSize - 180d);
                 var south = MapProjection.Wgs84MeterPerDegree * (180d - (row + 1) * tileSize);
