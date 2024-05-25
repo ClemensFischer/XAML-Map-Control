aeb3fd047fadb53afda93edf434f51a8327632d9
ClemensFischer <fischer.clemens@gmail.com>
2024-05-26 00:13:54 +0200

    Update MapPath.Avalonia.cs

--- MapControl/Avalonia/MapPath.Avalonia.cs
+++ MapControl/Avalonia/MapPath.Avalonia.cs
@@ -69,6 +69,7 @@ namespace MapControl
         protected void SetPathFigures(PathFigures pathFigures)
         {
             ((PathGeometry)Data).Figures = pathFigures;
+
             InvalidateGeometry();
         }
 
