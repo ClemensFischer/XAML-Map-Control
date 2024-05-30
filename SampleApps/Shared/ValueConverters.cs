ec13e13af771f12a3f14253e7326351c32ad8325
ClemensFischer <fischer.clemens@gmail.com>
2024-05-31 00:07:38 +0200

    Update ValueConverters.cs

--- SampleApps/Shared/ValueConverters.cs
+++ SampleApps/Shared/ValueConverters.cs
@@ -42,14 +42,24 @@ namespace SampleApplication
 
     public class MapHeadingToVisibilityConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, string language)
         {
             return (double)value != 0d;
         }
 
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, "");
+        }
+
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotSupportedException();
+            return ConvertBack(value, targetType, parameter, "");
         }
     }
 }
