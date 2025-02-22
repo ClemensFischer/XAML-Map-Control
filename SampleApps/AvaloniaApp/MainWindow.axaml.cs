15c5f3c3f18f640afeb48a6c720c96bc8172be5f
ClemensFischer <fischer.clemens@gmail.com>
2025-02-22 16:07:21 +0100

    Update MainWindow.axaml.cs

--- SampleApps/AvaloniaApp/MainWindow.axaml.cs
+++ SampleApps/AvaloniaApp/MainWindow.axaml.cs
@@ -10,14 +10,11 @@ namespace SampleApplication
 {
     public partial class MainWindow : Window
     {
-        static MainWindow()
-        {
-            //MapProjectionFactory.Instance = new MapControl.Projections.GeoApiProjectionFactory();
-            //TileImageLoader.Cache = new MapControl.Caching.ImageFileCache(TileImageLoader.DefaultCacheFolder);
-        }
-
         public MainWindow()
         {
+            //TileImageLoader.Cache = new MapControl.Caching.ImageFileCache(TileImageLoader.DefaultCacheFolder);
+            //Closed += (s, e) => (TileImageLoader.Cache as IDisposable)?.Dispose();
+
             InitializeComponent();
             AddTestLayers();
         }
