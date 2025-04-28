2efafcf92f4723a0d3a91cdf2a3a45d7a24846f8
ClemensFischer <fischer.clemens@gmail.com>
2025-04-28 08:27:53 +0200

    MapItemsControl ContextMenu

--- MapControl/WPF/MapItem.WPF.cs
+++ MapControl/WPF/MapItem.WPF.cs
@@ -30,37 +30,18 @@ namespace MapControl
 
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
-            e.Handled = true;
-        }
-
-        protected override void OnMouseRightButtonDown(MouseButtonEventArgs e)
-        {
-            e.Handled = true;
-        }
-
-        protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
-        {
-            if (ItemsControl.ItemsControlFromItemContainer(this) is MapItemsControl mapItemsControl)
+            if (Keyboard.Modifiers.HasFlag(ModifierKeys.Shift) &&
+                ItemsControl.ItemsControlFromItemContainer(this) is MapItemsControl mapItemsControl &&
+                mapItemsControl.SelectionMode == SelectionMode.Extended)
             {
-                if (mapItemsControl.SelectionMode == SelectionMode.Extended &&
-                    Keyboard.Modifiers.HasFlag(ModifierKeys.Shift))
-                {
-                    mapItemsControl.SelectItemsInRange(this);
-                }
-                else
-                {
-                    // Perform default mouse down item selection on mouse up.
-                    //
-                    base.OnMouseLeftButtonDown(e);
-                }
+                mapItemsControl.SelectItemsInRange(this);
+                e.Handled = true;
+            }
+            else
+            {
+                base.OnMouseLeftButtonDown(e);
             }
 
-            e.Handled = true;
-        }
-
-        protected override void OnMouseRightButtonUp(MouseButtonEventArgs e)
-        {
-            OnMouseLeftButtonUp(e);
         }
     }
 }
