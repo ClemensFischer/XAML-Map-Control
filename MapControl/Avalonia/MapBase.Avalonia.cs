31169d64dde867b07404da6b30b74c258b855db4
ClemensFischer <fischer.clemens@gmail.com>
2026-04-21 19:43:55 +0200

    Update MapBase.Avalonia.cs

--- MapControl/Avalonia/MapBase.Avalonia.cs
+++ MapControl/Avalonia/MapBase.Avalonia.cs
@@ -3,7 +3,6 @@ using Avalonia.Animation;
 using Avalonia.Animation.Easings;
 using Avalonia.Controls;
 using Avalonia.Controls.Documents;
-using Avalonia.Data;
 using Avalonia.Media;
 using Avalonia.Styling;
 using System.Threading;
